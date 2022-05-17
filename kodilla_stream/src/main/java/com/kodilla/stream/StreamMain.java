73997c8f1b5bad6f77adfdbcd10d625d7b35c4b6
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-05-17 20:16:18 +0200

    7.2 added Immutable Forum User

--- kodilla_stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla_stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -11,19 +12,24 @@ public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Welcome to module 7 - Stream");
 
-        PoemBeautifier poemBeautifier=new PoemBeautifier();
-        poemBeautifier.beautify("Ala ma kota",((text)-> text.toUpperCase()));
-        poemBeautifier.beautify("Ala ma kota",((text)->"ABC "+text+" ABC"));
-        poemBeautifier.beautify("Ala ma kota",((text)->"|||| "+text+" ||||"));
-        poemBeautifier.beautify("Ala ma kota",((text)->"\u001B[31m"+text+"\u001B[0m"));
-        poemBeautifier.beautify("Ala ma kota",((text)-> {
-        String resetColorCommand="\u001b[0m";
-        String output="";
-            for(int i=0;i<text.length();i++){
-              output+=  "\u001B[3"+(i%7)+"m"+text.charAt(i);
-            }
-            return output+resetColorCommand;
-        }));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+
+//        PoemBeautifier poemBeautifier=new PoemBeautifier();
+//        poemBeautifier.beautify("Ala ma kota",((text)-> text.toUpperCase()));
+//        poemBeautifier.beautify("Ala ma kota",((text)->"ABC "+text+" ABC"));
+//        poemBeautifier.beautify("Ala ma kota",((text)->"|||| "+text+" ||||"));
+//        poemBeautifier.beautify("Ala ma kota",((text)->"\u001B[31m"+text+"\u001B[0m"));
+//        poemBeautifier.beautify("Ala ma kota",((text)-> {
+//        String resetColorCommand="\u001b[0m";
+//        String output="";
+//            for(int i=0;i<text.length();i++){
+//              output+="\u001B[3"+(i%7)+"m"+text.charAt(i);
+//            }
+//            return output+resetColorCommand;
+//        }));
 
 
 //        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
