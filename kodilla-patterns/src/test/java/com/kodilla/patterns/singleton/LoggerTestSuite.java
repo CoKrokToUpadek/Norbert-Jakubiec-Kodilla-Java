2da3ab60c11847b032f4ea5b8d85fd8bd15fcd94
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-07-12 23:44:46 +0200

    15.1 logger task

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class LoggerTestSuite {
+
+    private static Logger logger;
+
+    @BeforeAll
+    public static void openSettingsFile() {
+
+       logger=Logger.INSTANCE;
+       logger.log("potato");
+
+    }
+
+
+    @Test
+    void testGetFileName() {
+        //Given
+        //When
+        String log = logger.getLastLog();
+        //Then
+        assertEquals("potato", log);
+    }
+
+}
